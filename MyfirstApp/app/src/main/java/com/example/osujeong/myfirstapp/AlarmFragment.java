202392cae4aa84a67505edc7f6e30bcde3fe42c9
dbehdwn <dbehdwn90@naver.com>
2018-11-26 13:38:55 +0900

    ads

---
+++ MyfirstApp/app/src/main/java/com/example/osujeong/myfirstapp/AlarmFragment.java
@@ -0,0 +1,26 @@
+package com.example.osujeong.myfirstapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AlarmFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        new AlarmActivity();
+
+        return inflater.inflate(R.layout.activity_alarm, container,false);
+
+
+
+
+    }
+}
