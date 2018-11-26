384bd4d81e3337d662df5053fe933199cc79c962
sujioh <ohsuji0518@gmail.com>
2018-11-26 17:41:36 +0900

    1

---
+++ MyfirstApp2 2/app/src/main/java/com/jh/myapplication4/AlarmFragment.java
@@ -0,0 +1,21 @@
+package com.jh.myapplication4;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AlarmFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_alaram, container, false);
+
+        return v;
+    }
+}
