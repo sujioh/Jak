85797db9ff9ef2bfdfe6fb0fbea6b489eadb9330
sujioh <ohsuji0518@gmail.com>
2018-12-04 15:17:04 +0900

    ds

---
+++ MyfirstApp2 2/app/src/main/java/com/jh/myapplication4/RingActivity.java
@@ -0,0 +1,34 @@
+package com.jh.myapplication4;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class RingActivity extends BroadcastReceiver {
+
+    TimePicker timePicker;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.start();
+
+    }
+}
+
