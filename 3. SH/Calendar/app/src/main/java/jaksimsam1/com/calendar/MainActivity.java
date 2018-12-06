071081e3200c0798cebb86e37c3056c2d8f933eb
sohee <surplusmarshell@gmail.com>
2018-12-06 17:03:56 +0900

    캘린더 형식

---
+++ 3. SH/Calendar/app/src/main/java/jaksimsam1/com/calendar/MainActivity.java
@@ -0,0 +1,30 @@
+package jaksimsam1.com.calendar;
+
+import android.content.Intent;
+import android.provider.CalendarContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Calendar_Activity";
+
+    private CalendarView mCalendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" +i2 + "/" + i;
+                Log.d(TAG, "onSelectedDayChange: date: " + date);
+            }
+        });
+    }
+}
